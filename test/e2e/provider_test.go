0979bb3355b394792fcb93d8ac72bdf554e6c2d7
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2025-05-13 16:06:24 +0900

    fix: fix test data

--- test/e2e/provider_test.go
+++ test/e2e/provider_test.go
@@ -7,13 +7,45 @@ import (
 	"time"
 
 	"github.com/bucketeer-io/go-server-sdk/pkg/bucketeer"
+	"github.com/bucketeer-io/go-server-sdk/pkg/bucketeer/model"
 	provider "github.com/bucketeer-io/openfeature-go-server-sdk/pkg"
 	"github.com/open-feature/go-sdk/openfeature"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	timeout = 10 * time.Second
+	timeout             = 20 * time.Second
+	tag                 = "go-server"
+	userID              = "bucketeer-go-server-user-id-1"
+	featureID           = "feature-go-server-e2e-string"
+	featureIDVariation1 = "value-1"
+	featureIDVariation2 = "value-2"
+	goalID              = "goal-go-server-e2e-1"
+	port                = 443
+
+	// Sdk Test
+	targetUserID                   = "bucketeer-go-server-user-id-1"
+	targetSegmentUserID            = "bucketeer-go-server-user-id-2" // This ID is configured in the segment user on the console
+	featureIDString                = "feature-go-server-e2e-string"
+	featureIDStringTargetVariation = featureIDStringVariation2
+	featureIDStringVariation1      = "value-1"
+	featureIDStringVariation2      = "value-2"
+	featureIDStringVariation3      = "value-3"
+
+	featureIDBoolean                = "feature-go-server-e2e-boolean"
+	featureIDBooleanTargetVariation = false
+
+	featureIDInt64                = "feature-go-server-e2e-int64"
+	featureIDInt64TargetVariation = featureIDInt64Variation2
+	featureIDInt64Variation1      = 3000000000
+	featureIDInt64Variation2      = -3000000000
+
+	featureIDFloat                = "feature-go-server-e2e-float"
+	featureIDFloatTargetVariation = featureIDFloatVariation2
+	featureIDFloatVariation1      = 2.1
+	featureIDFloatVariation2      = 3.1
+
+	featureIDJson = "feature-go-server-e2e-json"
 )
 
 // getEnvOrDefault gets an environment variable or returns a default value
@@ -36,19 +68,15 @@ func setupProvider(t *testing.T) *provider.Provider {
 		t.Fatalf("Required environment variables API_KEY and API_ENDPOINT must be set")
 	}
 
-	// Parse host and port from API_ENDPOINT
 	host := apiEndpoint
-	port := 443 // Default HTTPS port
 
 	options := []bucketeer.Option{
 		bucketeer.WithAPIKey(apiKey),
+		bucketeer.WithTag(tag),
 		bucketeer.WithHost(host),
 		bucketeer.WithPort(port),
 		bucketeer.WithEnableDebugLog(true),
 	}
-
-	tag := getEnvOrDefault("TAG", "go-server")
-	options = append(options, bucketeer.WithTag(tag))
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
@@ -65,6 +93,7 @@ func setupProvider(t *testing.T) *provider.Provider {
 func createEvalContext(userID string) openfeature.FlattenedContext {
 	evalCtx := map[string]any{
 		openfeature.TargetingKey: userID,
+		"attr-key":               "attr-value",
 	}
 
 	flatCtx := openfeature.FlattenedContext{}
@@ -80,25 +109,28 @@ func TestBooleanEvaluation(t *testing.T) {
 	ctx, cancel := context.WithTimeout(t.Context(), timeout)
 	defer cancel()
 	tests := []struct {
-		desc         string
-		userID       string
-		flagID       string
-		defaultValue bool
-		expected     bool
+		desc           string
+		userID         string
+		flagID         string
+		defaultValue   bool
+		expectedValue  bool
+		expectedReason model.EvaluationReason
 	}{
 		{
-			desc:         "Evaluation by default user",
-			userID:       "test-user-1",
-			flagID:       getEnvOrDefault("BOOLEAN_FLAG_ID", "test-boolean-flag"),
-			defaultValue: false,
-			expected:     true,
+			desc:           "Evaluation by default user",
+			userID:         "user-1",
+			flagID:         featureIDBoolean,
+			defaultValue:   false,
+			expectedValue:  true,
+			expectedReason: model.EvaluationReasonDefault,
 		},
 		{
-			desc:         "Evaluation by target user",
-			userID:       "target-user",
-			flagID:       getEnvOrDefault("BOOLEAN_FLAG_ID", "test-boolean-flag"),
-			defaultValue: false,
-			expected:     true,
+			desc:           "Evaluation by target user",
+			userID:         targetUserID,
+			flagID:         featureIDBoolean,
+			defaultValue:   false,
+			expectedValue:  featureIDBooleanTargetVariation,
+			expectedReason: model.EvaluationReasonTarget,
 		},
 	}
 
@@ -112,7 +144,8 @@ func TestBooleanEvaluation(t *testing.T) {
 
 			// Verify result is valid and matches expected value
 			assert.NotNil(t, result)
-			assert.Equal(t, tt.expected, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedValue, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedReason, result.Reason, "userID: %s, flagID: %s", tt.userID, tt.flagID)
 		})
 	}
 }
@@ -122,25 +155,36 @@ func TestStringEvaluation(t *testing.T) {
 	ctx, cancel := context.WithTimeout(t.Context(), timeout)
 	defer cancel()
 	tests := []struct {
-		desc         string
-		userID       string
-		flagID       string
-		defaultValue string
-		expected     string
+		desc           string
+		userID         string
+		flagID         string
+		defaultValue   string
+		expectedValue  string
+		expectedReason model.EvaluationReason
 	}{
 		{
-			desc:         "Evaluation by default user",
-			userID:       "test-user-2",
-			flagID:       getEnvOrDefault("STRING_FLAG_ID", "test-string-flag"),
-			defaultValue: "default-value",
-			expected:     "variation-1",
+			desc:           "Evaluation by default user",
+			userID:         "user-1",
+			flagID:         featureIDString,
+			defaultValue:   "default",
+			expectedValue:  featureIDStringVariation1,
+			expectedReason: model.EvaluationReasonDefault,
 		},
 		{
-			desc:         "Evaluation by target user",
-			userID:       "target-user",
-			flagID:       getEnvOrDefault("STRING_FLAG_ID", "test-string-flag"),
-			defaultValue: "default-value",
-			expected:     "target-variation",
+			desc:           "Evaluation by target user",
+			userID:         targetUserID,
+			flagID:         featureIDString,
+			defaultValue:   "default",
+			expectedValue:  featureIDStringTargetVariation,
+			expectedReason: model.EvaluationReasonTarget,
+		},
+		{
+			desc:           "Evaluation by Segment user",
+			userID:         targetSegmentUserID,
+			flagID:         featureIDString,
+			defaultValue:   "default",
+			expectedValue:  featureIDStringVariation3,
+			expectedReason: model.EvaluationReasonRule,
 		},
 	}
 
@@ -153,7 +197,8 @@ func TestStringEvaluation(t *testing.T) {
 			result := provider.StringEvaluation(ctx, tt.flagID, tt.defaultValue, evalCtx)
 
 			assert.NotNil(t, result)
-			assert.Equal(t, tt.expected, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedValue, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedReason, result.Reason, "userID: %s, flagID: %s", tt.userID, tt.flagID)
 		})
 	}
 }
@@ -163,25 +208,28 @@ func TestIntEvaluation(t *testing.T) {
 	ctx, cancel := context.WithTimeout(t.Context(), timeout)
 	defer cancel()
 	tests := []struct {
-		desc         string
-		userID       string
-		flagID       string
-		defaultValue int64
-		expected     int64
+		desc           string
+		userID         string
+		flagID         string
+		defaultValue   int64
+		expectedValue  int64
+		expectedReason model.EvaluationReason
 	}{
 		{
-			desc:         "Evaluation by default user",
-			userID:       "test-user-3",
-			flagID:       getEnvOrDefault("INT_FLAG_ID", "test-int-flag"),
-			defaultValue: 0,
-			expected:     100,
+			desc:           "Evaluation by default user",
+			userID:         "user-1",
+			flagID:         featureIDInt64,
+			defaultValue:   0,
+			expectedValue:  featureIDInt64Variation1,
+			expectedReason: model.EvaluationReasonDefault,
 		},
 		{
-			desc:         "Evaluation by target user",
-			userID:       "target-user",
-			flagID:       getEnvOrDefault("INT_FLAG_ID", "test-int-flag"),
-			defaultValue: 0,
-			expected:     200,
+			desc:           "Evaluation by target user",
+			userID:         targetUserID,
+			flagID:         featureIDInt64,
+			defaultValue:   0,
+			expectedValue:  featureIDInt64TargetVariation,
+			expectedReason: model.EvaluationReasonTarget,
 		},
 	}
 
@@ -194,7 +242,8 @@ func TestIntEvaluation(t *testing.T) {
 			result := provider.IntEvaluation(ctx, tt.flagID, tt.defaultValue, evalCtx)
 
 			assert.NotNil(t, result)
-			assert.Equal(t, tt.expected, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedValue, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedReason, result.Reason, "userID: %s, flagID: %s", tt.userID, tt.flagID)
 		})
 	}
 }
@@ -204,25 +253,28 @@ func TestFloatEvaluation(t *testing.T) {
 	ctx, cancel := context.WithTimeout(t.Context(), timeout)
 	defer cancel()
 	tests := []struct {
-		desc         string
-		userID       string
-		flagID       string
-		defaultValue float64
-		expected     float64
+		desc           string
+		userID         string
+		flagID         string
+		defaultValue   float64
+		expectedValue  float64
+		expectedReason model.EvaluationReason
 	}{
 		{
-			desc:         "Evaluation by default user",
-			userID:       "test-user-4",
-			flagID:       getEnvOrDefault("FLOAT_FLAG_ID", "test-float-flag"),
-			defaultValue: 0.0,
-			expected:     1.1,
+			desc:           "Evaluation by default user",
+			userID:         "user-1",
+			flagID:         featureIDFloat,
+			defaultValue:   0.0,
+			expectedValue:  featureIDFloatVariation1,
+			expectedReason: model.EvaluationReasonDefault,
 		},
 		{
-			desc:         "Evaluation by target user",
-			userID:       "target-user",
-			flagID:       getEnvOrDefault("FLOAT_FLAG_ID", "test-float-flag"),
-			defaultValue: 0.0,
-			expected:     2.2,
+			desc:           "Evaluation by target user",
+			userID:         targetUserID,
+			flagID:         featureIDFloat,
+			defaultValue:   0.0,
+			expectedValue:  featureIDFloatTargetVariation,
+			expectedReason: model.EvaluationReasonTarget,
 		},
 	}
 
@@ -235,7 +287,8 @@ func TestFloatEvaluation(t *testing.T) {
 			result := provider.FloatEvaluation(ctx, tt.flagID, tt.defaultValue, evalCtx)
 
 			assert.NotNil(t, result)
-			assert.Equal(t, tt.expected, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedValue, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedReason, result.Reason, "userID: %s, flagID: %s", tt.userID, tt.flagID)
 		})
 	}
 }
@@ -245,37 +298,30 @@ func TestObjectEvaluation(t *testing.T) {
 	ctx, cancel := context.WithTimeout(t.Context(), timeout)
 	defer cancel()
 	tests := []struct {
-		desc         string
-		userID       string
-		flagID       string
-		defaultValue interface{}
-		expected     interface{}
+		desc           string
+		userID         string
+		flagID         string
+		defaultValue   interface{}
+		expectedValue  interface{}
+		expectedReason model.EvaluationReason
 	}{
 		{
 			desc:   "Evaluation by default user",
-			userID: "test-user-5",
-			flagID: getEnvOrDefault("OBJECT_FLAG_ID", "test-object-flag"),
+			userID: "user-1",
+			flagID: featureIDJson,
 			defaultValue: map[string]interface{}{
 				"name":  "default-object",
 				"value": 0,
 			},
-			expected: map[string]interface{}{
-				"name":  "variation-1",
-				"value": 1,
-			},
+			expectedValue:  map[string]interface{}{"str": "str1", "int": "int1"},
+			expectedReason: model.EvaluationReasonDefault,
 		},
 		{
-			desc:   "Evaluation by target user",
-			userID: "target-user",
-			flagID: getEnvOrDefault("OBJECT_FLAG_ID", "test-object-flag"),
-			defaultValue: map[string]interface{}{
-				"name":  "default-object",
-				"value": 0,
-			},
-			expected: map[string]interface{}{
-				"name":  "target-variation",
-				"value": 2,
-			},
+			desc:           "Evaluation by target user",
+			userID:         targetUserID,
+			flagID:         featureIDJson,
+			expectedValue:  map[string]interface{}{"str": "str2", "int": "int2"},
+			expectedReason: model.EvaluationReasonTarget,
 		},
 	}
 
@@ -288,7 +334,8 @@ func TestObjectEvaluation(t *testing.T) {
 			result := provider.ObjectEvaluation(ctx, tt.flagID, tt.defaultValue, evalCtx)
 
 			assert.NotNil(t, result)
-			assert.Equal(t, tt.expected, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedValue, result.Value, "userID: %s, flagID: %s", tt.userID, tt.flagID)
+			assert.Equal(t, tt.expectedReason, result.Reason, "userID: %s, flagID: %s", tt.userID, tt.flagID)
 		})
 	}
 }
